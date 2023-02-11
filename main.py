5420398a339dfe7097f3a4893b2cd604c4430ad8
Wani29I <pachara31458@gmail.com>
2023-02-11 21:34:29 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,114 @@
+from flask import Flask, request
+from model_preprocess.function_lib import load_test_data, predictTestColor, create_color_bar
+from flask import render_template
+from flask import Flask
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://Wani29:Ice.31458@musicvector.xr7hdip.mongodb.net/?retryWrites=true&w=majority")
+
+app = Flask(__name__, template_folder='templates')
+musicvectorDb = client.MusicVector.musicvector
+
+mock = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 5, 1, 1, 1, 5, 5, 5, 5, 5, 5, 9, 9, 9, 9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 5, 5, 1, 1, 1, 1, 1, 1, 5, 5, 2, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 5, 1, 1, 5, 5, 5, 5, 2, 5, 5, 5, 1, 5, 9, 9, 9, 9, 9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 1, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 1, 1, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 2, 2, 5, 5, 2, 2, 2, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 1, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 1, 5, 5, 5, 2, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 2, 5, 5, 2, 2, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 2, 5, 5, 5, 5, 2, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+def store_music_vector(song_genre_array, filename):
+    
+    # check if already stored
+    prevSong = musicvectorDb.find_one({"song_genre_array": song_genre_array})
+    if prevSong : return
+    
+    genreCount = [0,0,0,0,0,0,0,0,0,0]
+    
+    unit = 1/len(song_genre_array)
+    
+    for genre in song_genre_array:
+        genreCount[genre] += unit
+    print(genreCount)
+    # print(filename)
+    
+    musicvectorDb.insert_one({
+        "file_name": filename,
+        "song_genre_array": song_genre_array,
+        "genre_percentage": genreCount
+        })
+    
+    return
+
+@app.route('/create')
+def create():
+    
+    # musicvectorDb.insert_one({0:0})
+    return {}
+
+@app.route('/')
+def index():
+    return render_template('mainTemplate.html')
+
+@app.route('/', methods=['POST'])
+def main_progress():
+    
+    if 'file' not in request.files:
+        return 'No file part in the request'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No file selected'
+
+    # Preprocess the file to extract features
+    x, sr = load_test_data(file)
+    
+    rawColor = dict(request.form)
+    
+    colorDict = {
+        0 : rawColor['color0'],
+        1 : rawColor['color1'],
+        2 : rawColor['color2'],
+        3 : rawColor['color3'],
+        4 : rawColor['color4'],
+        5 : rawColor['color5'],
+        6 : rawColor['color6'],
+        7 : rawColor['color7'],
+        8 : rawColor['color8'],
+        9 : rawColor['color9'],
+    }
+    # print(colorDict)
+    
+    color_list, genre_list = predictTestColor(x, sr, colorDict)
+        
+    store_music_vector(list(genre_list), file.filename)
+    
+    # print(len(color_list))
+    # print(color_list)
+    
+    
+    colors = color_list
+    
+    defaultRGB = list(colorDict.values())
+
+    # Example: return the color bar as a string
+    return render_template('mainTemplate.html', colors=colors, filename=file.filename, newDefaultRGB=defaultRGB)
+    
+
+@app.route('/test', methods=['GET'])
+def classify_file2():
+    
+    defaultRGB = [
+        "rgba(36, 108, 183, 1)",
+        "rgba(239, 234, 214, 1)",
+        "rgba(132, 202, 235, 1)",
+        "rgba(255, 0, 102, 1)",
+        "rgba(66, 66, 66, 1)",
+        "rgba(255, 159, 120, 1)",
+        "rgba(0, 57, 178, 1)",
+        "rgba(255, 106, 180, 1)",
+        "rgba(102, 204, 0, 1)",
+        "rgba(133, 194, 149, 1)",
+      ];
+    
+    colors = ['#000000']
+
+    # Example: return the color bar as a string
+    
+    return render_template('mainTemplate.html', colors=colors, filename='filename', newDefaultRGB=defaultRGB)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model_preprocess/function_lib.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import numpy as np
+import librosa
+import model_preprocess.music_gen_lib as mgl
+from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
+from keras.models import load_model
+import librosa
+
+MGCNN = mgl.Music_Genre_CNN(mgl.baseline_model_96)
+MGCNN.load_model("classify_model.h5")
+
+def load_test_data(song_file):
+    
+    X = []
+    SR = []
+    x, sr = librosa.load(song_file, sr=None)
+    X.append(x[:])
+    SR.append(sr)
+    return np.asarray(X), np.asarray(SR)
+
+def predictTestColor(testX, testSR, colorDict):
+
+    newTestX = mgl.batch_mel_spectrogram(testX, testSR)
+    newTestX = np.log(newTestX+1)
+    newTestX = newTestX[:, :, :, np.newaxis]
+    
+    predict_test1 = MGCNN.new_song_spectrogram_prediction_mid(newTestX[0])
+    
+    predict_test1 = [int(predict) for predict in predict_test1]
+    cmapColorList1 = [colorDict[predict] for predict in predict_test1]
+        
+    return cmapColorList1, predict_test1
+
+def create_color_bar(colors):
+    color_hex = []
+    for color in colors:
+        color_hex.append('#{:02x}{:02x}{:02x}'.format(int(color[0]*255), int(color[1]*255), int(color[2]*255)))
+    return color_hex
+    
+if __name__ == '__main__':
+    predictTestColor()
